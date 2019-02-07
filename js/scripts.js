b646308ae4ce68b9609432823f1543b950163034
Aya Abu <someone990@yahoo.com>
2019-02-06 23:35:08 -0800

    add or remove classes in js

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(function(){
+$("button#green").click(function(){
+  $("body").removeClass();
+  $("body").addClass("green-background");
+});
+$("button#blue").click(function(){
+  $("body").removeClass();
+  $("body").addClass("blue-background");
+});
+$("button#red").click(function(){
+  $("body").removeClass();
+  $("body").addClass("red-background");
+
+});
+
+$("button#clear").click(function(){
+  $("body").removeClass();
+
+
+})
+
+});
